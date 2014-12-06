ad93588b47377984f5445da2cdd0d0439682840d
Mario Villatoro <mvilla2208@gmail.com>
2014-12-05 21:39:59 -0600

    only registeres users can log in into app

--- MeetingTools/Views/LoginView.xaml.cs
+++ MeetingTools/Views/LoginView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Windows;
 using Microsoft.Phone.Controls;
 using GestureEventArgs = System.Windows.Input.GestureEventArgs;
+using Parse;
+
 
 namespace MeetingTools.Views
 {
@@ -15,5 +18,26 @@ namespace MeetingTools.Views
         {
             NavigationService.Navigate(new Uri("/Views/RegisterView.xaml", UriKind.Relative));
         }
+
+        private void TapLogin(object sender, GestureEventArgs e)
+        {
+            IsValidUser();
+        }
+
+        private async void IsValidUser()
+        {
+            ParseQuery<ParseObject> query = ParseObject.GetQuery("Users");
+            ParseObject getUser = await query.FirstOrDefaultAsync();
+            
+
+
+            if (UserBox.Text.Equals(getUser.Get<string>("username")) && PassBox.Password.Equals(getUser.Get<string>("password")))
+            //if(true)
+                NavigationService.Navigate(new Uri("/Views/MainPage.xaml", UriKind.Relative));
+            else
+                MessageBox.Show("Invalid User name or password");
+            
+        }
+
     }
 }
